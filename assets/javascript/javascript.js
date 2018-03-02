d230b2627f4e5e3f1e562f1df21c3d00c300841d
michael emmons <memmons18@gmail.com>
2018-03-02 10:04:42 -0500

    added modal to the buttons at the bottom of the page

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,52 @@
+var t=[{
+    name:"#menu",
+    class:"theMenu",
+    text:"this is where the menu will be displayed",
+    title:"menu"
+},{
+    name:"#reservation",
+    class:"Reservation",
+    text:"This is where a reservation form will show",
+    title:"Make a Reservation"
+    },{
+    name:"#catering",
+    class:"Reservation",
+    text:"this is where Catering information and form will show",
+    title:"Schedule a Catering Event"
+    }]
+
+$("#menu").on("click",function(){
+    $(".modal").css("display","block");
+
+    populateModal(0)
+})
+
+
+$("#reservation").on("click",function(){
+    $(".modal").css("display","block");
+    populateModal(1)
+
+})
+
+$("#catering").on("click",function(){
+    $(".modal").css("display","block");
+    populateModal(1)
+
+})
+
+function populateModal(num){
+    $(".modal-title").html(t[num].title);
+
+    var x = $("<div>");
+        x.addClass(t[num].class);
+        x.text(t[num].text);
+    $(".modal-body").append(x);
+
+}
+
+
+$(".close").on("click", function(){
+    $(".modal-body").empty()
+    $(".modal-title").empty();
+    $(".modal").css("display","none")
+})
